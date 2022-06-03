47f2162d5298200dd40dfac71a309b9ece908063
LasShukai <shu.kai@lasfactory.com>
2022-06-03 16:49:11 +0900

    imageSizeResponsive

--- pages/index.js
+++ pages/index.js
@@ -57,12 +57,44 @@ const Home = ({ items }) => {
                   scale: 1.03,
                 }}
               >
-                <motion.p className={styles.item_hideText}
-                variants={hiddenVarient}
-                initial='hidden'
-                whileHover='visible'
+                <motion.p
+                  className={styles.item_hideText}
+                  variants={hiddenVarient}
+                  initial="hidden"
+                  whileHover="visible"
                 >
-                    musit オリジナルZINE『(W)AVE』Vol.1　※よみ：ウェイヴ▼収録内容　※（）内はライター名巻頭特集：スピッツ30周年<br />・『スピッツ』&amp;『見っけ』クロスレビュー<br />・全アルバムレビュー<br />※寄稿者：翳目／Goseki／鮭いくら／高橋まりな／對馬拓／仲川ドイツ／中澤星児<br />※ジャケットイラスト：鮭いくらエッセイ<br />・さらりとした風（みくりや佐代子）<br />・音楽への不器用な愛を、呪いの歌姫に託して。（すなくじら）コラム<br />・歪み系エフェクターを購入する際に知っておくべきこと（Ot3）<br />・Good Music酒場探訪記 Vol.1 レレレノレコード（仲川ドイツ）<br />・ピンク・フロイドの叙情性（鈴木レイヤ）小説<br />・私、宇宙（安藤エヌ）<br />・destruction（中澤星児）<br />・CLOVER（高橋まりな）<br />・雯（鈴木レイヤ）▼仕様・サイズ：A5<br />・ページ数：50　※表紙含む<br />・色：オールカラー　※小説部分のみモノクロ※送料はZINEのみ購入の場合、一律¥300となります。
+                  musit
+                  オリジナルZINE『(W)AVE』Vol.1　※よみ：ウェイヴ▼収録内容　※（）内はライター名巻頭特集：スピッツ30周年
+                  <br />
+                  ・『スピッツ』&amp;『見っけ』クロスレビュー
+                  <br />
+                  ・全アルバムレビュー
+                  <br />
+                  ※寄稿者：翳目／Goseki／鮭いくら／高橋まりな／對馬拓／仲川ドイツ／中澤星児
+                  <br />
+                  ※ジャケットイラスト：鮭いくらエッセイ
+                  <br />
+                  ・さらりとした風（みくりや佐代子）
+                  <br />
+                  ・音楽への不器用な愛を、呪いの歌姫に託して。（すなくじら）コラム
+                  <br />
+                  ・歪み系エフェクターを購入する際に知っておくべきこと（Ot3）
+                  <br />
+                  ・Good Music酒場探訪記 Vol.1 レレレノレコード（仲川ドイツ）
+                  <br />
+                  ・ピンク・フロイドの叙情性（鈴木レイヤ）小説
+                  <br />
+                  ・私、宇宙（安藤エヌ）
+                  <br />
+                  ・destruction（中澤星児）
+                  <br />
+                  ・CLOVER（高橋まりな）
+                  <br />
+                  ・雯（鈴木レイヤ）▼仕様・サイズ：A5
+                  <br />
+                  ・ページ数：50　※表紙含む
+                  <br />
+                  ・色：オールカラー　※小説部分のみモノクロ※送料はZINEのみ購入の場合、一律¥300となります。
                 </motion.p>
                 <Image
                   src={cover}
@@ -92,6 +124,7 @@ const Home = ({ items }) => {
                   layout={"responsive"}
                   width={90}
                   height={90}
+ 
                 />
                 <h4 className={styles.notification}>何点買っても送料￥250</h4>
               </div>
@@ -100,21 +133,25 @@ const Home = ({ items }) => {
                 <div className={styles.grid_item} key={item.id}>
                   <Link href={item.permalink}>
                     <a target="_blank" rel="noreferrer">
-                        <div className={styles.item_imgContainer}>
-                            <motion.div className={styles.item_hideText}
-                            variants={hiddenVarient}
-                            initial="hidden"
-                            whileHover="visible"
-                            dangerouslySetInnerHTML={{__html:item.short_description}}
-                            ></motion.div>
-                            <Image
-                                className={styles.item_image}
-                                src={item.images[0].src}
-                                alt="itemPic"
-                                width={200}
-                                height={320}
-                                layout={"responsive"}
-                            />
+                      <div className={styles.item_imgContainer}>
+                        <motion.div
+                          className={styles.item_hideText}
+                          variants={hiddenVarient}
+                          initial="hidden"
+                          whileHover="visible"
+                          dangerouslySetInnerHTML={{
+                            __html: item.short_description,
+                          }}
+                        ></motion.div>
+                        <Image
+                          className={styles.item_image}
+                          src={item.images[0].src}
+                          alt="itemPic"
+                          width={200}
+                          height={320}
+                          layout={"responsive"}
+                          objectFit="contain"
+                        />
                       </div>
                       <p className={styles.item_title}>{item.name}</p>
                       <p className={styles.item_price}>
