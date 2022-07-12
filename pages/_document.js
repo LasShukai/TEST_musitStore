ccc051c550e097ea67f5b6b506095a28c2e83ac5
LasShukai <shu.kai@lasfactory.com>
2022-07-12 16:46:54 +0900

    vercel error

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ja">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
